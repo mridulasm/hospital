70860e5be7d8ffce11bef7ea1eb726ee2ae528ac
mridulasm <serahmridula@gmail.com>
2020-06-27 01:31:34 +0530

    minor fix

--- register.php
+++ register.php
@@ -1,47 +1,30 @@
 <?php
-   
-    $p_name = $_POST['p_name'];
-    $p_address = $_POST['p_address'];
-    $p_no = $_POST['p_no'];
-    $p_age = $_POST['p_age'];
-    
-    // Create connection
-    $conn = mysqli_connect("localhost", "root", "");
-    // Check connection
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-      
-      // Create database
-      $sql = "CREATE DATABASE DataBase1";
-      if (mysqli_query($conn, $sql)) 
-      {
-        echo "Database created successfully";
-      } else {
-        echo "Error creating database: " . mysqli_error($conn);
-      }
-      // sql to create table
-    $sql = "CREATE TABLE Patients (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital_management";
 
-    if (mysqli_query($conn, $sql)) {
-      echo "Table Patients created successfully";
-    } else {
-      echo "Error creating table: " . mysqli_error($conn);
-    }
-    
-    $insert = "Insert into Patients (p_name,p_address,p_no,p_age)
-    values ('$p_name', '$p_address', '$p_no', '$p_age')";
-    
-    if ($conn->query($insert)) 
-      echo "New record created successfully";
-     else 
-      echo "Error: " . $insert . "<br>" . mysqli_error($conn);
-    
+$p_username = $_POST['p_username'];
+$p_name = $_POST['p_name'];
+$p_age = $_POST['p_age'];
+$p_address = $_POST['p_address'];
+$p_no = $_POST['p_no'];
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO patient_details (p_username,p_name,p_age,p_address,p_no)
+    VALUES ('$p_username', '$p_name','$p_age','$p_address','$p_no')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
