f4e9294fe4cffe56eb848b2ff6e47a160b64c2d5
mridulasm <serahmridula@gmail.com>
2020-06-28 11:31:25 +0530

    minor fix

--- drregister.php
+++ drregister.php
@@ -16,7 +16,7 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO patient_details (d_username,d_name,d_department,d_no)
+$sql = "INSERT INTO doctor_details (d_username,d_name,d_department,d_no)
     VALUES ('$d_username','$d_name','$d_department','$d_no')";
 
 if (mysqli_query($conn, $sql)) {
