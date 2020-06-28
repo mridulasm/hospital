aca5d91a357142b6ea9a83557265105bd02bbfc3
mridulasm <serahmridula@gmail.com>
2020-06-29 02:04:06 +0530

    minor fix

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ $dbname = "hospital_management";
 $p_username = $_POST['p_username'];
 $p_name = $_POST['p_name'];
 $p_age = $_POST['p_age'];
-$p_address = $_POST['p_address'];
+$p_password = $_POST['p_password'];
 $p_no = $_POST['p_no'];
 
 // Create connection
@@ -17,8 +17,8 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO patient_details (p_username,p_name,p_age,p_address,p_no)
-    VALUES ('$p_username', '$p_name','$p_age','$p_address','$p_no')";
+$sql = "INSERT INTO patient_details (p_username,p_name,p_age,p_passsword,p_no)
+    VALUES ('$p_username', '$p_name','$p_age','$p_password','$p_no')";
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
